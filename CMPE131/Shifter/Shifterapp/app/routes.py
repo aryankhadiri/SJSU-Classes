a4fa968efc06510e4aafdbd49552745ad1607b44
Aryan Khadiri <aryankhadiri@gmail.com>
2019-10-30 11:35:58 -0700

    Working on the databases. Basic pages are done

---
+++ CMPE131/Shifter/Shifterapp/app/routes.py
@@ -0,0 +1,67 @@
+from app import Shifter 
+from app import db
+from app.forms import LoginForm, EmployeeForm, LogoutForm, EditViewForm, RegisterForm
+from app.models import Organization, Employee
+from flask import render_template, flash, redirect, url_for
+from flask import request
+from flask_login import current_user, login_user, login_required, logout_user
+from werkzeug.urls import url_parse
+
+@Shifter.route('/', methods = ['GET', 'POST'])
+def login():
+    formLogin = LoginForm()
+    if formLogin.Register.data and formLogin.is_submitted():
+        return redirect(url_for('register'))
+
+    elif formLogin.is_submitted():
+        user = Employee.query.filter_by(email=formLogin.Username.data).first()
+        if user is None or not user.check_password(formLogin.Password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        #login_user(user, remember=formLogin.RememberMe.data)
+        # return to page before user got asked to login
+        #next_page = request.args.get('next')
+        #if not next_page or url_parse(next_page).netloc != '':
+        #    next_page = url_for('choose')
+
+        #return redirect(next_page)
+    title = "Shifter Scheduling Application"
+    return render_template('login.html', title=title, formLogin=formLogin)
+
+
+@Shifter.route("/addemployee")
+def addemployee():
+    title = "Add employee to Shifter"
+    formEmployee = EmployeeForm()
+    formLogout = LogoutForm()
+    return render_template("addemployee.html", title=title, formEmployee=formEmployee, formLogout=formLogout)
+
+
+@Shifter.route("/choose")
+def chooseToDo():
+    title = "ChooseToDo"
+    formLogout = LogoutForm()
+    formEditView = EditViewForm()
+    return render_template("choose.html", title = title, formLogout = formLogout, formEditView = formEditView)
+@Shifter.route("/register", methods = ['GET', 'POST'])
+def register():
+    title = "Register Organization"
+    formRegister = RegisterForm()
+    if formRegister.validate_on_submit():
+        Organization = Organization(Name=formRegister.name_company.data, 
+                                    email=formRegister.email.data,
+                                    typeofbusiness=formRegister.type_company.data,
+                                    address=formRegister.address.data,
+                                    Phonenumber=formRegister.Business_phone_number.data)
+
+        Employee = Employee(fname = formRegister.manager_namef.data,
+                            lname = formRegister.manager_namel.data,
+                            email = formRegister.email.data,
+                            phone_number=formRegister.Manager_phone_number.data)
+                            #FIX ME! How to add employee into organization as the key
+
+
+    return render_template("register.html", title=title, formRegister=formRegister)
+
+if __name__ == '__main__':
+    Shifter.run()
--- CMPE131/myBlogExampleFLASK/myBlog/app/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import TextAreaField,StringField, PasswordField, BooleanField, SubmitField, IntegerField
-
-class TopCities(FlaskForm):
-    city_name = StringField("City Name:")
-    city_rank = IntegerField("City Rank:")
-    is_visited = BooleanField("Visited")
-    comments = TextAreaField("Comments")
-    submit = SubmitField("Submit")
